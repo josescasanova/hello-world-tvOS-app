a825c68f7e86f1b0e37a90bffab4a179c769691c
Jose Casanova <josescasanova@gmail.com>
2015-10-10 20:34:40 -0400

    Initial Commit

---
+++ hello world/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hello world
+//
+//  Created by Jose Casanova on 10/10/15.
+//  Copyright © 2015 Jose Casanova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
